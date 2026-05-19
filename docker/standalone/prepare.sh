53de408e4c4e4e1c728994564b591af78728ebcd
Vladislav Bakin <vladislav@bakin.me>
2026-05-19 12:02:50 +0100

    Fix docker image dir structure

--- docker/standalone/prepare.sh
+++ docker/standalone/prepare.sh
@@ -15,8 +15,8 @@ mkdir -p "$BUILD_DIR/dist"
 
 cp "$BACKEND_APP_DIR/package.json" "$BUILD_DIR/package.json"
 cp "$BACKEND_APP_DIR/bun.lock" "$BUILD_DIR/bun.lock"
-cp -R "$BACKEND_APP_DIR/dist/" "$BUILD_DIR/dist/"
+cp -R "$BACKEND_APP_DIR/dist/." "$BUILD_DIR/dist/"
 rm -rf "$BUILD_DIR/dist/types"
 
 mkdir "$BUILD_DIR/dist/public"
-cp -R "$WEB_APP_DIR/dist/" "$BUILD_DIR/dist/public/"
+cp -R "$WEB_APP_DIR/dist/." "$BUILD_DIR/dist/public/"
