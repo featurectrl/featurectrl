e72752af9a7fc063dc1f4688950acdceba7a060c
Vladislav Bakin <vladislav@bakin.me>
2026-05-24 10:24:32 +0100

    Deployment

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.featurectrl.io',
   integrations: [
     starlight({
       title: 'featurectrl docs',
